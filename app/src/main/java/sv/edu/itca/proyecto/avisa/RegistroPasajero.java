e861e1288427b8374c0f80c0291e98f08c9b2ffb
herberth30 <55860302+herberth30@users.noreply.github.com>
2019-10-28 18:59:16 -0600

    falta darle funcionalidad a los botones de agregar foto y aqui va el nuevo splash

--- app/src/main/java/sv/edu/itca/proyecto/avisa/RegistroPasajero.java
+++ app/src/main/java/sv/edu/itca/proyecto/avisa/RegistroPasajero.java
@@ -18,7 +18,7 @@ public class RegistroPasajero extends AppCompatActivity {
     public void buscarFotoRegistroPasajero(View view) {
         AlertDialog.Builder builder4 = new AlertDialog.Builder(this);
         LayoutInflater inflater = this.getLayoutInflater();
-        builder4.setView(inflater.inflate(R.layout.activity_opciones_fotografia_conductor, null)).setPositiveButton("Cancelar", new DialogInterface.OnClickListener() {
+        builder4.setView(inflater.inflate(R.layout.activity_opciones_fotografia_pasajero, null)).setPositiveButton("Cancelar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int id) {
                 dialog.cancel();
