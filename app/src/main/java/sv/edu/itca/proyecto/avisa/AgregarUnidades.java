03adc331f8f3f11b64e04f7eff86913eb823bea0
Cl1maco <mauricio99climacogomez@gmail.com>
2019-11-02 16:58:54 -0600

    Agregue el menu (en conductor login)y unos fondos de prueba.
    Mapas de Rutas Ahuacha y Metapan
    
    Cuadros de dialogo personalizados
    
    Enlaze a Pefil Pasajero

--- app/src/main/java/sv/edu/itca/proyecto/avisa/AgregarUnidades.java
+++ app/src/main/java/sv/edu/itca/proyecto/avisa/AgregarUnidades.java
@@ -3,12 +3,25 @@ package sv.edu.itca.proyecto.avisa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AgregarUnidades extends AppCompatActivity {
+    private Button AgregarUnidad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_unidades);
+
+        AgregarUnidad = (Button)findViewById(R.id.btnPropietarioAgregarUnidad);
+
+        AgregarUnidad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Codigo de Agregar",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/sv/edu/itca/proyecto/avisa/InicioPropietario.java
+++ app/src/main/java/sv/edu/itca/proyecto/avisa/InicioPropietario.java
@@ -2,13 +2,70 @@ package sv.edu.itca.proyecto.avisa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class InicioPropietario extends AppCompatActivity {
+    private ImageButton Unidades, Favoritas, Agregar, Perfil, Config, Salir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_propietario);
+
+        Unidades=findViewById(R.id.btnPropietarioUnidades);
+        Favoritas=findViewById(R.id.btnPropietarioFavoritos);
+        Agregar=findViewById(R.id.btnPropietarioAgregar);
+        Perfil=findViewById(R.id.btnPropietarioPerfil);
+        Config=findViewById(R.id.btnPropietarioConfig);
+        Salir=findViewById(R.id.btnPropietarioSalir);
+
+        Unidades.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Activity unidades Propietario",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        Favoritas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Unidades Favoritas",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Agregar Unidades",Toast.LENGTH_LONG).show();
+                Intent rpasajero = new Intent(InicioPropietario.this, AgregarUnidades.class);
+                startActivity(rpasajero);
+            }
+        });
+
+        Perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Perfil Propietario",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Config.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Configuracion",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
